bea4c5aacb47515b983b1ede7066de299db7b3af
Jackie Ho <jackie.ho16@stjohns.edu>
2020-03-27 18:57:37 -0400

    created scoreboard, disables buttons when the user submits their answer

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,21 @@
+let correct = 0;
 let answered = [];
 let quiz_data;
 let name;
 
+
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector("#user_form").onsubmit = () => {
+        // resets the answered questions everytime a new quiz is generated
+        answered = []
+        correct = 0;
+
         display_name();
         display_quiz();
         return false;
     }
 })
+
 
 let get_api_data = async (url) => {
     let response = await fetch(url);
@@ -16,12 +23,14 @@ let get_api_data = async (url) => {
     generate_question(quiz_data);
 }
 
+
 function display_name() {
     name = document.querySelector("#name_input").value;
 
     document.querySelector("#name").textContent = name;
     document.querySelector("#score").textContent = "Score: 0/0";
 }
+
 
 function display_quiz() {
     let quiz_choice = document.querySelector("#quiz_choice").value;
@@ -36,31 +45,35 @@ function display_quiz() {
         url = url + "quiz2"
     }
 
-    // resets the answered questions everytime a new quiz is generated
-    answered = []
     get_api_data(url);
 }
 
 
 function generate_question(data) {
+    // re-enables submit button for quiz type once the new quiz is generated
+    // fixes the problem of getting an answer right and then pressing submit, if you click it before the 3 seconds is up,
+    // a new question will get generated even though you already generated a new question
+    document.querySelector("#quiz_type_submit").disabled = false;
     if (answered.length < data.length) {
         let random = random_num(data.length);
         while (answered.includes(random)) {
             random = random_num(data.length);
         }
         let question = data[random];
+        answered.push(random);
 
         remove_current_quiz();
         display_question(question);
-        answered.push(random);
     } else {
         console.log("End of quiz");
     }
 }
 
+
 function random_num(length) {
     return Math.floor(Math.random() * length);
 }
+
 
 function remove_current_quiz() {
     try {
@@ -69,6 +82,7 @@ function remove_current_quiz() {
 
     }
 }
+
 
 function display_question(data) {
     create_element("body", "div", "class", "quiz container", "");
@@ -84,8 +98,11 @@ function display_question(data) {
 
     document.querySelector("#choices").onsubmit = () => {
         let choices = document.querySelectorAll(".choice")
+        // disable the submit button
         for (choice of choices) {
             if (choice.checked) {
+                document.querySelector(".submit").disabled = true;
+                document.querySelector("#quiz_type_submit").disabled = true;
                 if (choice.value == data.answer) {
                     // handle correct answer
                     correct_answer();
@@ -99,6 +116,7 @@ function display_question(data) {
     }
 }
 
+
 function create_element(parent, element, attribute, attribute_value, content) {
     let node = document.createElement(element);
     let text = document.createTextNode(content);
@@ -110,6 +128,7 @@ function create_element(parent, element, attribute, attribute_value, content) {
     return node;
 }
 
+
 function create_choices(choice) {
     let radio_button = create_element("#choices", "input", "class", "choice", "");
     radio_button.setAttribute("type", "radio");
@@ -118,23 +137,35 @@ function create_choices(choice) {
     create_element("#choices", "label", "class", "choice_text", choice)
 }
 
+
 function correct_answer() {
     let feedback_arr = ["Great Job", "Excellent", "Awesome", "You're A Superstar", "Keep Going, You're Doing Amazing", "Amazing", "Fantastic", "Good Job", "Brilliant", "Genius"]
     let random = random_num(feedback_arr.length);
     let feedback = `${feedback_arr[random]} ${name}!`;
     create_element(".quiz", "h1", "id", "correct_feedback", feedback);
+    update_score(true);
     setTimeout(() => {
         generate_question(quiz_data);
     }, 3000);
-    console.log('hello');
 }
+
 
 function wrong_answer(reason) {
     create_element(".quiz", "form", "id", "incorrect_form", "");
     create_element("#incorrect_form", "h1", "id", "incorrect_feedback", reason);
     create_element("#incorrect_form", "button", "class", "incorrect_feedback_button btn btn-info", "I Understand");
+    update_score(false);
     document.querySelector("#incorrect_form").onsubmit = () => {
         generate_question(quiz_data);
         return false;
     }
+}
+
+
+function update_score(bool) {
+    let score = document.querySelector("#score");
+    if (bool == true) {
+        correct++;
+    }
+    score.textContent = `Score: ${correct}/${answered.length}`;
 }
