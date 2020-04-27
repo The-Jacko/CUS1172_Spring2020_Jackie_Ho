899f5c27aa8710b7e96ea6d0a532c1eaa272de81
Jackie Ho <jackie.ho16@stjohns.edu>
2020-04-27 17:29:23 -0400

    changed quiz-list to be an object rather than an array of objects

--- Quiz Project - Milestone 2/data/quiz-list.js
+++ Quiz Project - Milestone 2/data/quiz-list.js
@@ -1,13 +1,14 @@
-var quiz_list = [{
+var quiz_list = {
+    1: {
         id: "1",
         title: "Web Development",
         description: "Quiz on web development basics, such as HTML, CSS, and Javascript."
     },
-    {
+    2: {
         id: "2",
         title: "General Computer Science",
         description: "Quiz on computer science fundamentals. Includes topics on Java and Python."
     }
-];
+};
 
 module.exports = quiz_list;
