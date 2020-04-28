fba95c83bc9ad5919a7c585a88d35987fec8152c
Jackie Ho <jackie.ho16@stjohns.edu>
2020-04-27 22:34:16 -0400

    testing deploying to heroku

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 
 const api_routes = require("./routes/api_routes.js")
 
+app.get("/", function (req, res) {
+    res.send("<h1>Homepage</h1>")
+})
+
 app.use("/api", api_routes);
 
 // listen for server
